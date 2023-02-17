27a9c7f8e6556a1df9ad5269fcab5b1626a83e4e
Artem <67517973+KingArthur-IT@users.noreply.github.com>
2023-02-17 23:40:03 +0300

    modals

--- js/header.js
+++ js/header.js
@@ -34,8 +34,113 @@ document.querySelector('#selectedCity').addEventListener('keypress', (e) => {
 });
 
 //login
+const emailInput = document.querySelector('#emailLogin')
+const passwordInput = document.querySelector('#passwordLogin')
+const errorField = document.querySelector('#loginError')
+const emailValidator = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
 document.querySelector('#loginBtn').addEventListener('click', () => {
+    errorField.classList.add('d-none')
+
+    if (!emailValidator.test(emailInput.value)) {
+        errorField.classList.remove('d-none')
+        errorField.innerHTML = 'Не верный email'
+        return
+    }
+    if (!passwordInput.value) {
+        errorField.classList.remove('d-none')
+        errorField.innerHTML = 'Не верный пароль'
+        return
+    }
+
+    //убрать кнопку войти и показать иконку профиля
     document.querySelector('.header__login-btn').classList.add('d-none')
     document.querySelector('.profile').classList.remove('d-none')
+    //hide modal
     $('#loginModal').modal('hide');
+
+    //set user data
+    // document.querySelector('#usernameData').innerHTML = 'User name'
+    // document.querySelector('#useremailData').innerHTML = 'User email'
 });
+
+//register
+const nameRegInput = document.querySelector('#nameRegister')
+const emailRegInput = document.querySelector('#emailRegister')
+const passwordRegInput = document.querySelector('#passwordRegister')
+const repeatPasswordRegInput = document.querySelector('#repeatPasswordRegister')
+const errorFieldReg = document.querySelector('#registerError')
+
+document.querySelector('#registerBtn').addEventListener('click', () => {
+    errorFieldReg.classList.add('d-none')
+
+    if (nameRegInput.value.length < 4) {
+        errorFieldReg.classList.remove('d-none')
+        errorFieldReg.innerHTML = 'Имя должно быть не короче 4 символов'
+        return
+    }
+    if (!emailValidator.test(emailRegInput.value)) {
+        errorFieldReg.classList.remove('d-none')
+        errorFieldReg.innerHTML = 'Не верный email'
+        return
+    }
+    if (passwordRegInput.value.length < 6) {
+        errorFieldReg.classList.remove('d-none')
+        errorFieldReg.innerHTML = 'Пароль не должен быть короче 6 символов'
+        return
+    }
+    if (passwordRegInput.value !== repeatPasswordRegInput.value) {
+        errorFieldReg.classList.remove('d-none')
+        errorFieldReg.innerHTML = 'Пароли не совпадают'
+        return
+    }
+
+    //убрать кнопку войти и показать иконку профиля
+    document.querySelector('.header__login-btn').classList.add('d-none')
+    document.querySelector('.profile').classList.remove('d-none')
+    //hide modal
+    $('#registerModal').modal('hide');
+
+    //set user data
+    // document.querySelector('#usernameData').innerHTML = 'User name'
+    // document.querySelector('#useremailData').innerHTML = 'User email'
+});
+
+document.querySelector('#openLoginModal').addEventListener('click', () => {
+    $('#registerModal').modal('hide');
+    $('#loginModal').modal('show');
+});
+
+document.querySelector('#openRegisterModal').addEventListener('click', () => {
+    $('#loginModal').modal('hide');
+    $('#registerModal').modal('show');
+});
+
+document.querySelector('#openForgotModal').addEventListener('click', () => {
+    $('#loginModal').modal('hide');
+    $('#forgotPasswordModal').modal('show');
+});
+
+//forgot password
+const emailForgotInput = document.querySelector('#emailForgotPass')
+const errorForgotField = document.querySelector('#forgotError')
+
+document.querySelector('#forgotBtn').addEventListener('click', () => {
+    errorForgotField.classList.add('d-none')
+
+    if (!emailValidator.test(emailForgotInput.value)) {
+        errorForgotField.classList.remove('d-none')
+        errorForgotField.innerHTML = 'Не верный email'
+        return
+    }
+
+    //hide modal
+    $('#forgotPasswordModal').modal('hide');
+    $('#resetPasswordModal').modal('show');
+});
+
+//logout
+document.querySelector('#logout').addEventListener('click', () => {
+    document.querySelector('.header__login-btn').classList.remove('d-none')
+    document.querySelector('.profile').classList.add('d-none')
+})
