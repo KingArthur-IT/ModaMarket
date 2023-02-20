dc1615b26b1e5e354221cb6190790b91f2e345f0
Artem <67517973+KingArthur-IT@users.noreply.github.com>
2023-02-20 20:23:45 +0300

    edits

--- js/catalog.js
+++ js/catalog.js
@@ -1,7 +1,13 @@
+//sort dropdown
 const sortDropdown = document.querySelector('.catalog__sort-dropdown')
 
 sortDropdown.querySelectorAll('.dropdown-item').forEach(el => 
     el.addEventListener('click', () => {
         sortDropdown.querySelector('button').innerHTML = el.innerHTML
     })    
-)
+);
+
+//mobile filters modal
+document.querySelector('.catalog__mob-filters').addEventListener('click', () => {
+    $('#filtersModal').modal('show');
+});
